c99eb4d8ebcd003e6c35e97b82c9e6a5de6b628d
gmestat <156371600+gmestat@users.noreply.github.com>
2024-01-11 15:17:23 -0600

    Create file.R

---
+++ Basic bs4Dash shiny app/file.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(highcharter)
+
+
+View(data[1:100,])
+colnames(data)
+table(data$Country_Code)
+table(data$Country_Name)
+table(data$Year)
+table(data$Attribute_Description)
+table(data$Unit_Description)
+table(data$Value)
+table(data$Seen_On)
+
+filtered_data_time_chart <- data %>% 
+  filter(
+    Commodity_Description == "Almonds, Shelled Basis",
+    Attribute_Description == "Exports",
+    Country_Name == "Greece"
+  ) %>%
+  arrange(Year, Value)
+filtered_data_time_chart
+
+
+hchart(filtered_data,
+       "column",
+       hcaes(x = "Year", y = "Value"))
+
+
+
+filtered_data_pie_chart <- data %>% 
+  filter(
+    Country_Name == "Greece",
+    Attribute_Description == "Imports",
+    Year == "1984"
+  )
+
+
+hchart(
+  filtered_data_pie_chart,
+  "pie",
+  hcaes(x = "Commodity_Description", y = "Value")
+)
